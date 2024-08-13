ec333fb222ad6f6f0a1d8cfa2ab581f51453bf64
dayyam.arice.con <dayyam.arice.con@phonepe.com>
2024-08-13 12:06:55 +0530

    fourth commit

--- main.js
+++ main.js
@@ -1,26 +1,19 @@
+
 const express = require('express');
 const app = express();
-const port = 3000;
 
-// Middleware to parse JSON bodies
 app.use(express.json());
-//app.use(express.text({ type: '*/*' }));
 
-// POST route
 app.post('/submit-data', (req, res) => {
   const receivedData = req.body;
-  console.log('Received data:', receivedData);
-
-  //const decodedData = Buffer.from(receivedData, 'base64').toString('utf-8');
-
-  //console.log(decodedData);
-
-
-  // Respond with a success message
   res.json({ message: 'Data received successfully', data: receivedData });
+
+    //const decodedData = Buffer.from(receivedData, 'base64').toString('utf-8');
+
 });
 
-// Start the server
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
+
